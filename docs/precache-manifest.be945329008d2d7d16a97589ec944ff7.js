256128c9bfe009fe7fbf3c3f113769a403553c11
rishabh3112 <rishabh31121999@gmail.com>
2019-03-24 17:00:35 +0530

    build: with banner only

---
+++ docs/precache-manifest.be945329008d2d7d16a97589ec944ff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89be0ebfcb2760c06c9",
+    "url": "/js/about.1383eeaa.js"
+  },
+  {
+    "revision": "a3541856c4d56698a307",
+    "url": "/css/app.eafd6760.css"
+  },
+  {
+    "revision": "a3541856c4d56698a307",
+    "url": "/js/app.1b33f042.js"
+  },
+  {
+    "revision": "f236584e893b96bea4b4",
+    "url": "/js/chunk-vendors.d0b317e0.js"
+  },
+  {
+    "revision": "fdba4ace0c06c5ed820c248bcce675f4",
+    "url": "/img/logo.fdba4ace.png"
+  },
+  {
+    "revision": "2fef1991d2779ad8c6ee94ba73181d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
