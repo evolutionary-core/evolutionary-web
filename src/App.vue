fb8f62f454566d2c1af91cb4a24781c406679316
rishabh3112 <rishabh31121999@gmail.com>
2019-03-24 15:06:09 +0530

    chore: mdc

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,78 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="app">
+    <hello-world class="app__hello"
+      title="Welcome to your Vue MDC Adapter App"
+      :vmaLinks="vmaLinks"
+      :mdcLinks="mdcLinks">
+    </hello-world>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    data () {
+      return {
+        vmaLinks: [
+          {
+            title: 'Documentation',
+            url: 'https://stasson.github.io/vue-mdc-adapter'
+          },
+          {
+            title: 'GitHub',
+            url: 'https://github.com/stasson/vue-mdc-adapter'
+          },
+          {
+            title: 'Twitter',
+            url: 'https://twitter.com/vuemdc'
+          },
+          {
+            title: 'Chat',
+            url: 'https://gitter.im/vue-mdc-adapter/Lobby'
+          }
+        ],
+        mdcLinks: [
+          {
+            title: 'Documentation',
+            url: 'https://material.io/components/web/'
+          },
+          {
+            title: 'GitHub',
+            url: 'https://github.com/material-components/material-components-web'
+          },
+          {
+            title: 'Guidelines',
+            url: 'https://material.io/guidelines'
+          },
+          {
+            title: 'Awesome Material Components',
+            url: 'https://github.com/webdenim/awesome-material-components'
+          }
+        ]
+      }
+    },
+    components: { HelloWorld }
   }
-}
+</script>
+
+<style lang="scss">
+  // First, set the value for variable
+  $mdc-typography-font-family: "Roboto Mono", monospace;
+
+  // Then, import required files
+  @import "@material/typography/mixins";
+
+  html {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    @include mdc-typography(body2);
+
+    width: 100%;
+    min-height: 100%;
+    margin: 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueMDCAdapter from 'vue-mdc-adapter'
+
+Vue.use(VueMDCAdapter)
 
 Vue.config.productionTip = false
 
